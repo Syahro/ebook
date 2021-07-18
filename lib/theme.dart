abcab6171ba1e2f57d6c74b183ad9027ee69326f
Arief Syahroni <riefsyah31@gmail.com>
2021-07-18 10:31:10 +0700

    Add some assets and new page to support MainPage

---
+++ lib/theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final double defaultMargin = 30.0;
+
+Color whiteColor = Color(0XFFFFFFFF);
+Color blackColor = Color(0XFF2D2D2D);
+Color greyColor = Color(0XFFBDBDBD);
+Color lightGreyColor = Color(0XFFF3F3F3);
+Color greenColor = Color(0XFF098B5C);
+
+TextStyle titleTextStyle = GoogleFonts.poppins(
+  fontSize: 16,
+  color: blackColor,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle subtitleTextStyle = GoogleFonts.poppins(
+  fontSize: 14,
+  color: greyColor,
+  fontWeight: FontWeight.w400,
+);
